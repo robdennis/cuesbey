652cd4c88d046203e5c1fbfcb5a7df9010d509cf
Rob Dennis <rdennis@gmail.com>
2012-10-29 15:03:57 -0400

    slightly less trivial unittest for making the skeleton of a cube given a sort spec

--- cuesbey_main/cube_viewer/static/js/src/sortCube.js
+++ cuesbey_main/cube_viewer/static/js/src/sortCube.js
@@ -1,25 +1,62 @@
-var heuristic_names = [];
+var heuristicNames = [];
 
-function sorter(serializedCube) {
+function isArray(object) {
+    return Object.prototype.toString.call( object ) === '[object Array]';
+}
+
+function turnSortSpecIntoSortedCubeSkeleton(sortSpec) {
+    sortSpec = sortSpec || {};
+
+    var sortedCube = {};
+
+    _recurseSpec = function (subSpec, subCube) {
+        $.each(subSpec, function(_subName, _subSpec) {
+            console.log('looking at:', _subName, _subSpec);
+            if (jQuery.isEmptyObject(_subSpec)) {
+                // check that we don't have an information key like a "_sort"
+                console.log('empty subspec, making list');
+                subCube[_subName] = [];
+            } else {
+                console.log('recursing with:', _subName, _subSpec);
+                var _recurseCube = {};
+                subCube[_subName] = _recurseCube;
+                _recurseSpec(_subSpec, _recurseCube);
+            }
+        })
+    };
+
+    _recurseSpec(sortSpec, sortedCube);
+
+    return sortedCube;
+}
+
+function sorter(serializedCube, sortSpec, defaultSort) {
+    var sortedCube = turnSortSpecIntoSortedCubeSkeleton(sortSpec);
+
+    defaultSort = defaultSort || function(card_a, card_b) {
+        return card_a['name'] > card_b['name']
+    };
 
     $.each(serializedCube, function(index, value) {
         console.log('sorting card:', index, value);
-        handle_cube_card(value);
+        handle_cube_card(value, sortedCube, sortSpec);
     });
 
-    heuristic_names.sort()
+    heuristicNames.sort();
+
+    return sortedCube;
 }
 
-function handle_cube_card(card) {
+function handle_cube_card(card, sortedCube) {
 
-
+    // if there are heuristics, note what they are
     $.each(card['heuristics'] || {}, function(name, modifications) {
-        console.log('HEURISTICS!!!');
-        if (jQuery.inArray(name, heuristic_names)==-1) {
-            heuristic_names.push(name);
+        if (jQuery.inArray(name, heuristicNames)==-1) {
+            heuristicNames.push(name);
         }
+    });
 
-    })
+
 }
 
 

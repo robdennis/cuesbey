1444d989d8729702c0312a370ca1c413eac5238f
Rob Dennis <rdennis@gmail.com>
2013-02-08 22:58:16 -0500

    tweaked controller to be a bit less fake

--- cuesbey_main/cube_viewer/tests/js/unit/controllersSpec.js
+++ cuesbey_main/cube_viewer/tests/js/unit/controllersSpec.js
@@ -3,9 +3,18 @@
 /* jasmine specs for controllers go here */
 
 describe('service', function() {
-    beforeEach(module('cubeViewer.controllers'));
+    beforeEach(module('cubeViewer.controllers', 'cubeViewer.services'));
 
-    describe("cubeContentsCtrlTest", function() {
+    describe("CubeContentsCtrl", function() {
+
+        var ctrl;
+
+        beforeEach(inject(function($rootScope, $controller) {
+            var scope = $rootScope.$new();
+            ctrl = $controller("CubeContentsCtrl", {$scope: scope});
+        }));
+
+
         it("should work", function() {
 
         });

b421bfe2a2b92f637ff88a0d7427464503901de1
Rob Dennis <rdennis@gmail.com>
2013-04-07 19:46:28 -0400

    update to production config

--- cuesbey_main/cuesbey/settings/production.py
+++ cuesbey_main/cuesbey/settings/production.py
@@ -2,14 +2,4 @@ from cuesbey.settings import *
 
 DEBUG = False 
 TEMPLATE_DEBUG = False
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lookout',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': '',
-        'PORT': '',
-        }
-}
+STATIC_ROOT = '/home/ubuntu/static'
